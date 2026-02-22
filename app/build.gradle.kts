9b9e95b48cefb13d278545cd9173df30ac9139eb
Alexsik76 <o.sikorskyi.vn@gmail.com>
2026-02-22 13:18:39 +0200

    feat: complete Lab 2 with fragment navigation and UI updates

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
     }
 
     defaultConfig {
-        applicationId = "com.example.lb1"
+        applicationId = "com.example.lb2"
         minSdk = 24
         targetSdk = 36
         versionCode = 1
